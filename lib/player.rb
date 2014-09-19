3486aca8e22bd15b10481f960a9ce2e79aaf55eb
App Academy Student <student@appacademy.io>
2014-09-19 18:04:31 -0400

    working on player spec

--- lib/player.rb
+++ lib/player.rb
@@ -1,1 +1,30 @@
-class
+class Player
+  attr_accessor :name
+  attr_reader :pot, :hand, :bank
+
+  def initialize(name, hand = Hand.new, bank)
+    @name = name
+    @hand = hand
+    @pot = 0
+    @bank = bank
+    @active = false
+  end
+
+  def discard(indices)
+    indices.each do |index|
+      @hand.cards[index] = nil
+    end
+    @hand.cards.compact!
+  end
+
+  def fold
+    @active = false
+  end
+
+  def active?
+    @active
+  end
+
+  protected
+  attr_reader :active
+end
--- spec/Player_spec.rb
+++ spec/Player_spec.rb
@@ -1,17 +1,35 @@
 require 'player'
 
 describe Player do
-
+  let(:hand) { double(:hand) }
+  subject(:player) { Player.new("Name", hand, 100) }
   it 'should have a name' do
     expect(player.name.nil?).to be false
   end
 
-  it 'should have a hand'
-  it 'should have a pot'
-  it 'should have bank account'
-  it 'should be able to discard cards'
-  it 'should be able fold'
-  it 'should be able to see'
+  it 'pot should start with 0' do
+    expect(player.pot).to eq(0)
+  end
+
+  it 'should have bank account' do
+    expect(player.bank > 0).to be true
+  end
+
+  it 'should be able to discard cards' do
+    hand.stub(:cards).and_return([1,2,3,4,5])
+    player.discard([0,1,2])
+    expect(player.hand.cards).to eq([4, 5])
+  end
+
+  it 'should be able fold' do
+    player.fold
+    expect(player.active?).to be false
+  end
+
+  it 'should be able to see' do
+    # player.call
+    # expect(player.)
+  end
   it 'should be able to raise'
   it 'player shouldnt be able to discard more than three cards'
 
